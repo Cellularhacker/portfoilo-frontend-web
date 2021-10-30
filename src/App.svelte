6a4615e77572f192f6b9bc0eb4c2f7407a8b2002
Cellularhacker <wva3cdae@gmail.com>
2021-10-30 15:55:26 +0900

    testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import Project from "./components/Project.svelte";
+
 	export let name: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		<Project name="Project H" from="2021-10-01" to="2021-11-31" description="Something..." id="1" imageUrl="/favicon.png"/>
+	</p>
 </main>
 
 <style>
---
+++ src/components/Project.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let id: number;
+    export let name: string;
+    export let description: string;
+    export let from: string;
+    export let to: string;
+    export let imageUrl: string;
+</script>
+
+<div id="{id}" class="project" style="background-image: url({imageUrl})">
+    <h3>{name}</h3>
+    {from} ~ {to}<br/>
+    <small>{description}</small><br/>
+</div>
+
+<style lang="css">
+.project {
+    width: 300px;
+    height: 600px;
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+    background-blend-mode: soft-light;
+}
+</style>
